ca41478f46cd95a0214995f426e468670b313d77
Fredrik Lönnqvist <flonkation@gmail.com>
2021-12-12 15:02:15 +0200

    day7 p1

---
+++ day7/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Flonka/advent-of-code-2021/input"
+)
+
+func main() {
+	d := input.ReadCommaSeparatedInts("input")
+
+	ds := sort.IntSlice(d)
+
+	ds.Sort()
+
+	min := ds[0]
+	max := ds[len(ds)-1]
+	consumption := calculateConsumption(min, ds)
+	for i := min + 1; i <= max; i++ {
+
+		c := calculateConsumption(i, ds)
+
+		if c <= consumption {
+			consumption = c
+		} else {
+			// if trend goes other way, it should never come back
+			fmt.Println("Min cons:", consumption, i)
+			break
+		}
+	}
+
+	fmt.Println("Fuel Consumption:", consumption)
+}
+
+func calculateConsumption(pos int, data []int) int {
+
+	c := 0
+	for i := 0; i < len(data); i++ {
+		diff := data[i] - pos
+		if diff < 0 {
+			diff = -diff
+		}
+		c += diff
+	}
+
+	return c
+}
--- input/input.go
+++ input/input.go
@@ -2,9 +2,11 @@ package input
 
 import (
 	"bufio"
+	"bytes"
 	"io"
 	"log"
 	"os"
+	"strconv"
 )
 
 func OpenFile(fileName string) io.ReadCloser {
@@ -26,4 +28,40 @@ func ReadLinesInFile(fileName string) []string {
 	}
 
 	return outputLines
+}
+
+// ReadCommaSeparatedInts assumes the first line in a file has all the integers comma separated
+func ReadCommaSeparatedInts(fiePath string) []int {
+	r := OpenFile(fiePath)
+	defer r.Close()
+
+	s := bufio.NewScanner(r)
+
+	s.Split(func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+
+		commaIndex := bytes.IndexByte(data, ',')
+		newLineIndex := bytes.IndexByte(data, '\n')
+		if atEOF {
+			return len(data), nil, nil
+		}
+		i := commaIndex
+
+		if commaIndex == -1 && newLineIndex != -1 {
+			i = newLineIndex
+		}
+
+		return i + 1, data[:i], nil
+
+	})
+
+	var data []int
+	for s.Scan() {
+		i, err := strconv.Atoi(s.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		data = append(data, i)
+	}
+
+	return data
 }
