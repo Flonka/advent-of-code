e8be1fb465d521e38677bcf89cb65bb4d078cb5a
flonka <flonkation@gmail.com>
2022-12-18 01:26:04 +0200

    day3 wip2

--- 2022/day3/main.go
+++ 2022/day3/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strings"
+	"unicode"
 
 	"github.com/Flonka/advent-of-code/input"
 )
@@ -11,34 +12,55 @@ func main() {
 
 	s := input.OpenFileBuffered("input")
 
+	var prioMap map[int]bool = make(map[int]bool)
 	for s.Scan() {
 		line := s.Text()
 		l := len(line)
 		c1 := line[:(l / 2)]
 		c2 := line[(l / 2):]
 
-		getCommonPriority(c1, c2)
-		break
+		ps := getCommonPriorities(c1, c2)
+
+		for _, i := range ps {
+			prioMap[i] = true
+		}
 	}
+
+	var sum int
+	for k := range prioMap {
+		sum += k
+	}
+
+	fmt.Println("task1", sum)
 
 }
 
-func getCommonPriority(c1 string, c2 string) int {
+func getCommonPriorities(c1 string, c2 string) []int {
 
-	fmt.Println("c1", c1)
-	fmt.Println("c2", c2)
-	var psum int
+	// fmt.Println("c1", c1)
+	// fmt.Println("c2", c2)
+	var prios []int
 	for _, v := range c1 {
 
 		if strings.ContainsRune(c2, v) {
 
-			fmt.Println(v)
-			fmt.Printf("%s\n", v)
-			psum += int(v)
+			var priodiff int
+			if unicode.IsUpper(v) {
+				// 65 -27 =
+				priodiff = 38
+			} else {
+				// 97 - 1
+				priodiff = 96
+			}
+
+			// fmt.Println(string(v))
+			prio := int(v) - priodiff
+			// fmt.Println(prio)
+			prios = append(prios, prio)
 		}
 
 	}
-	fmt.Println(psum)
 
-	return psum
+	return prios
+
 }
