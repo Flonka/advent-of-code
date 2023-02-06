0f0d0eb0c4674280d13488f703cb7e171c4a7dfa
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-02-06 16:51:35 +0200

    day9 wip1

--- 2022/day9/main.go
+++ 2022/day9/main.go
@@ -9,20 +9,75 @@ import (
 	"github.com/Flonka/advent-of-code/input"
 )
 
+type vec2 struct {
+	x int
+	y int
+}
+
+type state struct {
+	head vec2
+	tail vec2
+}
+
 func main() {
 	s := input.OpenFileBuffered("input")
+
+	ropeState := state{}
+
+	visited := make(map[vec2]int)
+
+	// 0,0 visited (startpos)
+	visited[vec2{}]++
 
 	for s.Scan() {
 		line := s.Text()
 		splits := strings.Split(line, " ")
 
-		direction := splits[0]
+		dirString := splits[0]
 		amount, err := strconv.Atoi(splits[1])
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		fmt.Println(direction, amount)
+		direction := getDir(dirString)
+
+		newTailPositions := updateState(&ropeState, direction, amount)
+		for _, p := range newTailPositions {
+			visited[p]++
+		}
+
 	}
+
+	for k, v := range visited {
+		fmt.Println(k, v)
+	}
+}
+
+func getDir(s string) vec2 {
+	v := vec2{}
+	switch s {
+	case "U":
+		v.y = 1
+	case "D":
+		v.y = -1
+	case "L":
+		v.x = -1
+	case "R":
+		v.x = 1
+	default:
+		fmt.Println("Unhandled dir", s)
+		os.Exit(1)
+	}
+
+	return v
+}
+
+// updateState updates the state and returns new visited tail positions
+func updateState(s *state, direction vec2, amount int) []vec2 {
+
+	tails := make([]vec2, 0, amount)
+	// fmt.Println(direction, amount)
+
+	return tails
 }
