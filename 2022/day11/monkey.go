f914783f37dec27d304c4e9d05cff24b2a8721ac
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-03-06 17:32:01 +0200

    monkey construction

--- 2022/day11/monkey.go
+++ 2022/day11/monkey.go
@@ -1,9 +1,49 @@
 package day11
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
 type Monkey struct {
 	items        []int
 	operation    func(int) int
 	inspectCount int
+}
+
+/* NewMonkeyFromLines takes line input to create a new Monkey , the structure looks like this example:
+Starting items: 98, 97, 98, 55, 56, 72
+ Operation: new = old * 13
+ Test: divisible by 11
+   If true: throw to monkey 4
+   If false: throw to monkey 7
+*/
+func NewMonkeyFromLines(lines []string) Monkey {
+
+	// fmt.Println(lines)
+	// items
+	itemString := lines[0][strings.Index(lines[0], ":")+1:]
+	itemStrings := strings.Split(itemString, ",")
+	items := make([]int, 0, len(itemStrings))
+	for _, s := range itemStrings {
+		v, err := strconv.Atoi(strings.TrimSpace(s))
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		items = append(items, v)
+	}
+	// fmt.Println(lines[0], "->", items)
+
+	// Operation 
+	opLine := lines[1]
+	fmt.Println(opLine)
+
+	return Monkey{
+		items: items,
+	}
 }
 
 type test struct {
--- 2022/day11/run/main.go
+++ 2022/day11/run/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/Flonka/advent-of-code/2022/day11"
+	"github.com/Flonka/advent-of-code/input"
 )
 
 func main() {
@@ -11,8 +13,8 @@ func main() {
 	// Read monkeys
 	monkeys := readData("input.txt")
 
-	for _, m := range monkeys {
-		fmt.Println(m)
+	for i, m := range monkeys {
+		fmt.Println("Monkey", i, m)
 	}
 
 	/*
@@ -25,5 +27,22 @@ func main() {
 }
 
 func readData(f string) []day11.Monkey {
+	s := input.OpenFileBuffered(f)
 
+	monkeys := make([]day11.Monkey, 0, 10)
+	for s.Scan() {
+		l := s.Text()
+
+		if strings.HasPrefix(l, "Monkey") {
+			monkeyLines := make([]string, 0, 5)
+			for i := 0; i < 5; i++ {
+				s.Scan()
+				monkeyLines = append(monkeyLines, s.Text())
+			}
+
+			monkeys = append(monkeys, day11.NewMonkeyFromLines(monkeyLines))
+		}
+	}
+
+	return monkeys
 }
