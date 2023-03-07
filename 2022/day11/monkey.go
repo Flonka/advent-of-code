524ccf95fbf6b431ac25738a432c0e4514735bfe
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-03-07 08:29:25 +0200

    day11 wip monkey creation

--- 2022/day11/monkey.go
+++ 2022/day11/monkey.go
@@ -9,8 +9,37 @@ import (
 
 type Monkey struct {
 	items        []int
-	operation    func(int) int
+	operation    operation
 	inspectCount int
+	test         test
+}
+type operation struct {
+	fun  func(int, int) int
+	oldA bool
+	oldB bool
+	a    int
+	b    int
+}
+
+func (o *operation) Eval(old int) int {
+	var a, b int
+	if o.oldA {
+		a = old
+	} else {
+		a = o.a
+	}
+	if o.oldB {
+		b = old
+	} else {
+		b = o.b
+	}
+	return o.fun(a, b)
+}
+
+type test struct {
+	parameter     int
+	successTarget int
+	failureTarget int
 }
 
 /* NewMonkeyFromLines takes line input to create a new Monkey , the structure looks like this example:
@@ -37,17 +66,48 @@ func NewMonkeyFromLines(lines []string) Monkey {
 	}
 	// fmt.Println(lines[0], "->", items)
 
-	// Operation 
+	// Operation
 	opLine := lines[1]
-	fmt.Println(opLine)
+	operatorStrings := strings.Split(opLine[strings.Index(opLine, "= ")+2:], " ")
+	fmt.Println(operatorStrings)
+	operator := operatorStrings[1]
+	var opFun func(int, int) int
+	switch operator {
+	case "*":
+		opFun = multOp
+	case "+":
+		opFun = addOp
+
+	default:
+		fmt.Println("Unhandled operator", operator)
+		os.Exit(1)
+	}
+	// Check if "old" for A / B
+	opA, err := strconv.Atoi(operatorStrings[0])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	opB, err := strconv.Atoi(operatorStrings[2])
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
 	return Monkey{
 		items: items,
+		operation: operation{
+			fun: opFun,
+			a:   opA,
+			b:   opB,
+		},
 	}
 }
 
-type test struct {
-	parameter     int
-	successTarget int
-	failureTarget int
+func multOp(a, b int) int {
+	return a * b
+}
+
+func addOp(a, b int) int {
+	return a + b
 }
