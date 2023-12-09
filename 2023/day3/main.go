0a0528bc985caaf0d3bfa848b8708f3a5eb09f38
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-12-09 12:49:29 +0200

    multi pass , finding symbols first

--- 2023/day3/main.go
+++ 2023/day3/main.go
@@ -1,9 +1,39 @@
 package main
 
-import "github.com/Flonka/advent-of-code/cli"
+import (
+	"bufio"
+	"fmt"
+	"unicode"
+
+	"github.com/Flonka/advent-of-code/cli"
+	"github.com/Flonka/advent-of-code/input"
+)
 
 func main() {
 
 	cli.Default()
+
+	// s := input.OpenFileBuffered("input.txt")
+	fileReader := input.OpenFile("input.txt")
+	defer fileReader.Close()
+	r := bufio.NewReader(fileReader)
+
+	// Pass 1 - find all symbol positions
+	for {
+		line, err := r.ReadString('\n')
+		// Symbol is not digit, and not . and not newline, Readstring includes delimiter
+
+		for _, s := range line {
+			if s == '.' || unicode.IsDigit(s) || s == '\n' {
+				continue
+			}
+			// Print symbol
+			fmt.Println(fmt.Sprintf("'%v'", string(s)))
+		}
+
+		if err != nil {
+			break
+		}
+	}
 
 }
