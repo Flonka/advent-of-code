b3408242ec439b6a6a57725ca2c030f5c887392d
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2023-12-09 13:03:07 +0200

    store map of symbols

--- 2023/day3/main.go
+++ 2023/day3/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/Flonka/advent-of-code/cli"
 	"github.com/Flonka/advent-of-code/input"
+	"github.com/Flonka/advent-of-code/spatial"
 )
 
 func main() {
@@ -18,22 +19,33 @@ func main() {
 	defer fileReader.Close()
 	r := bufio.NewReader(fileReader)
 
+	symbols := make(map[spatial.DiscretePos2D]string, 50)
+
+	var row, col int
 	// Pass 1 - find all symbol positions
 	for {
 		line, err := r.ReadString('\n')
 		// Symbol is not digit, and not . and not newline, Readstring includes delimiter
-
+		col = 0
 		for _, s := range line {
+
 			if s == '.' || unicode.IsDigit(s) || s == '\n' {
 				continue
 			}
 			// Print symbol
-			fmt.Println(fmt.Sprintf("'%v'", string(s)))
+			// fmt.Println(fmt.Sprintf("'%v'", string(s)))
+			symbols[spatial.DiscretePos2D{X: col, Y: row}] = string(s)
+
+			col++
 		}
 
 		if err != nil {
 			break
 		}
+
+		row++
 	}
+
+	fmt.Println(symbols)
 
 }
