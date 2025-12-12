b225888355dd5a64d3f56d581d102747d41b6bdc
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-12-12 17:02:25 +0200

    reading data

--- 2025/day5/main.go
+++ 2025/day5/main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"strconv"
 
 	"github.com/Flonka/advent-of-code/input"
 )
 
 func main() {
-	readData()
+	ranges, ids := readData()
+	fmt.Println(len(ranges), len(ids))
 }
 
 func readData() ([]input.Range, []int) {
 	lines := input.ReadLinesInFile("input.txt")
 	emptyLine := false
 
+	ranges := make([]input.Range, 0, 100)
 	ids := make([]int, 0, 100)
 
 	for _, l := range lines {
 		if len(l) == 0 {
 			emptyLine = true
+			continue
 		}
 		if emptyLine {
-			i, _ := strconv.Atoi(l)
+			i, err := strconv.Atoi(l)
+			if err != nil {
+				log.Fatalln(err)
+			}
 			ids = append(ids, i)
 		} else {
-			fmt.Println("read range", l)
+			r, err := input.NewRangeFromString(l)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			ranges = append(ranges, r)
 		}
 	}
-	return nil, ids
+	return ranges, ids
 }
