24fbc748165a0a64f2a2101f92812df53fd8976e
flonnqvi <fredrik.lonnqvist@crosskey.fi>
2025-12-21 01:48:15 +0200

    pairs from closest

--- 2025/day8/main.go
+++ 2025/day8/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log/slog"
+	"math"
 	"strings"
 
 	"github.com/Flonka/advent-of-code/cli"
@@ -16,6 +17,43 @@ func main() {
 
 	slog.Info("boxes", "length", len(boxes))
 	// connect together the 1000 pairs of junction boxes which are closest together. Afterward, what do you get if you multiply together the sizes of the three largest circuits?
+
+	pairs := make([]pair, 0, 1000)
+	// Find the closest box for every box
+	for i, b := range boxes {
+		cb, l := findClosest(b, i, boxes)
+		p := pair{i, cb, l}
+		pairs = append(pairs, p)
+		slog.Debug("Pair added", "pair", p)
+	}
+
+	slog.Info("pair length", "pairs", len(pairs))
+}
+
+// findClosest returns index for closest vector in the collection ,skipping vIndex
+func findClosest(v mathutils.Vec3[int], vIndex int, collection []mathutils.Vec3[int]) (int, float64) {
+	min := math.Inf(1)
+	minIdx := -1
+	for i, b := range collection {
+
+		if i == vIndex {
+			continue
+		}
+		dv := mathutils.NewV3Sub(&b, &v)
+		d := dv.Length()
+		if d < min {
+			minIdx = i
+			min = d
+		}
+	}
+	return minIdx, min
+}
+
+// pair connect two indices
+type pair struct {
+	a        int
+	b        int
+	distance float64
 }
 
 func readInput() []mathutils.Vec3[int] {
--- mathutils/vec3.go
+++ mathutils/vec3.go
@@ -16,3 +16,12 @@ type Vec3[T Number] struct {
 func (v *Vec3[T]) Length() float64 {
 	return math.Sqrt(float64(v.X*v.X + v.Y*v.Y + v.Z*v.Z))
 }
+
+// NewV3Sub returns a new vector (v - v2)
+func NewV3Sub[T Number](v, v2 *Vec3[T]) Vec3[T] {
+	return Vec3[T]{
+		X: v.X - v2.X,
+		Y: v.Y - v2.Y,
+		Z: v.Z - v2.Z,
+	}
+}
